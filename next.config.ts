1f3403a0405ebb05e55dbbd81df9929c5bdd1bd6
Rene <rene.vucko@roakon.eu>
2025-11-26 21:46:05 +0100

    Add docker for local development

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  watchOptions: {
+    pollIntervalMs: 1000,
+  }
 };
 
 export default nextConfig;
